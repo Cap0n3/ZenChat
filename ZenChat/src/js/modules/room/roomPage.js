2f0606db094fdfb1f02d4b8951c86adc6ca45194
Cap0n3 <cap0n3@protonmail.com>
2023-12-27 20:28:03 +0100

    Refactored roomPage.js => mod1

--- ZenChat/src/js/modules/room/roomPage.js
+++ ZenChat/src/js/modules/room/roomPage.js
@@ -10,20 +10,21 @@ export function roomPage() {
     const chatMessageInput = document.getElementById("chatMessageInput");
     const chatMessageSend = document.getElementById("chatMessageSend");
     const onlineUsersSelector = document.getElementById("onlineUsersSelector");
-    
-    // Event listeners
-    chatMessageSend.addEventListener("click", handleSubmit);
-    chatMessageInput.addEventListener("keyup", function(e) {
-        if (e.key === "Enter") {  // enter key
-            handleSubmit(e);
-        }
-    });
+
+    // focus 'chatMessageInput' when user opens the page
+    chatMessageInput.focus();
+
+    const form = document.getElementById("chatInputForm");
+
+    form.addEventListener("submit", handleSubmit);
+
 
     // Handle form submission
     function handleSubmit(e) {
         e.preventDefault();
         
         const message = chatMessageInput.value.trim();
+
         if (!message) return;
 
         // Check for exessive message length
@@ -37,6 +38,7 @@ export function roomPage() {
         ws.send(JSON.stringify({
             "message": message
         }));
+        
         chatMessageInput.value = "";
     }
 
@@ -68,9 +70,11 @@ export function roomPage() {
                     handleUserLeave(data);
                     break;
                 case "private_message":
-                    handlePrivateMessage(data);
+                    // TODO: handle private message
+                    //handlePrivateMessage(data);
                     break;
                 case "private_message_delivered":
+                    // TODO: handle private message delivery
                     // handle private message delivery
                     break;
                 default:
@@ -145,26 +149,6 @@ export function roomPage() {
         if (oldOption !== null) oldOption.remove();
     }
 
-    // focus 'chatMessageInput' when user opens the page
-    chatMessageInput.focus();
-
-    // submit if the user presses the enter key
-    chatMessageInput.onkeyup = function(e) {
-        if (e.keyCode === 13) {  // enter key
-            chatMessageSend.click();
-        }
-    };
-
-    chatMessageSend.onclick = function() {
-        if (chatMessageInput.value.length === 0) return;
-        // Forward the message to the WebSocket
-        chatSocket.send(JSON.stringify({
-            "message": chatMessageInput.value,
-        }));
-        // Clear the 'chatMessageInput'
-        chatMessageInput.value = "";
-    };
-
 
     /**
      * Creates a new message element.
@@ -213,111 +197,12 @@ export function roomPage() {
         return messageElement.outerHTML;
     }
 
-    //=== Websocket communication ===//
-
-    // let chatSocket = null;
-
-    // function connect(){
-    //     chatSocket = new WebSocket("ws://" + window.location.host + "/ws/chat/" + roomName + "/");
-        
-    //     chatSocket.onopen = function(e) {
-    //         console.log("Successfully connected to the Websocket.")
-    //     }
-
-    //     chatSocket.onclose = function(e) {
-    //         console.log("Websocket connection closed unexpectedly. Trying to reconnect in 2s...");
-    //         setTimeout(function() {
-    //             console.log("Reconnecting...");
-    //             connect();
-    //         }, 2000);
-    //     }
-
-    //     chatSocket.onmessage = function(e) {
-    //         const data = JSON.parse(e.data);
-    //         console.log(data);
-
-    //         switch(data.type) {
-    //             case "chat_message":
-    //                 scrollerInner.insertAdjacentHTML(
-    //                     "beforeend",    
-    //                     `
-    //                     <li id="msg_${data.nonce}">
-    //                         <div class="message-container">
-    //                             <img class="user-avatar" src="${data.avatar}" alt="avatar">
-    //                             <h3 class="chat-user">${data.user} <span class="timestamp">${data.timestamp}</span></h3>
-    //                             <div class="message-content">${data.message}</div>
-    //                         </div>
-    //                     </li>
-    //                     `    
-    //                 );
-    //                 scrollDown(chatFeed);
-    //                 break;
-    //             case "user_list":
-    //                 for (let i = 0; i < data.users.length; i++) {
-    //                     onlineUsersSelectorAdd(data.users[i]);
-    //                 }
-    //                 break;
-    //             case "user_join":
-    //                 scrollerInner.insertAdjacentHTML(
-    //                     "beforeend", 
-    //                     //"<li><span class='chat-joined-user'><i>" + data.user + "</span> joined the room.</i></li>"
-    //                     `
-    //                     <li>
-    //                         <div class="message-container">
-    //                             <div class="chat-joined-user">--- ${data.user} joined the room ---</div>
-    //                         </div>
-    //                     </li>
-    //                     `
-    //                 );
-    //                 onlineUsersSelectorAdd(data.user);
-    //                 scrollDown(chatFeed);
-    //                 break;
-    //             case "user_leave":
-    //                 // chatLog.value += data.user + " left the room.\n";
-    //                 scrollerInner.insertAdjacentHTML(
-    //                     "beforeend", 
-    //                     //"<li><span class='chat-joined-user'><i>" + data.user + "</span> joined the room.</i></li>"
-    //                     `
-    //                     <li>
-    //                         <div class="message-container">
-    //                             <div class="chat-joined-user">--- ${data.user} left the room ---</div>
-    //                         </div>
-    //                     </li>
-    //                     `
-    //                 );
-    //                 onlineUsersSelectorRemove(data.user);
-    //                 scrollDown(chatFeed);
-    //                 break;
-    //             case "private_message":
-    //                 // chatLog.value += "PM from " + data.user + ": " + data.message + "\n";
-    //                 break;
-    //             case "private_message_delivered":
-    //                 // chatLog.value += "PM to " + data.target + ": " + data.message + "\n";
-    //                 break;
-    //             default:
-    //                 //console.error("Unknown message type !");
-    //                 console.error("Unknown message type ! \n(data.type = " + data.type + ")");
-    //                 break;
-
-    //         }
-    //     }
-
-        // Scroll 'chatLog' to the bottom
-        // chatLog.scrollTop = chatLog.scrollHeight;
-
-    //     chatSocket.onerror = function(err) {
-    //         console.log("Websocket encountered an error: " + err.message);
-    //         console.log("Closing the socket");
-    //         chatSocket.close();
-    //     }    
-    // }
-
-    connect();
-
     // TODO: implement private messaging
     // onlineUsersSelector.onchange = function() {
     //     chatMessageInput.value = "/pm " + onlineUsersSelector.value + " ";
     //     onlineUsersSelector.value = null;
     //     chatMessageInput.focus();
     // }
+
+    connect();
 }
